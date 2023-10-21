4fdb0cd76a9f8bf7db1123c33cd000add3820320
jamesbush <jjbush@uvm.edu>
2023-10-21 16:34:10 -0400

    MY COMP DYING

--- MyFindMusic/ContentView.swift
+++ MyFindMusic/ContentView.swift
@@ -20,15 +20,70 @@ struct ContentView: View {
     }
 }
 
-struct MapView: View {
-    @State var region = MKCoordinateRegion(
-        center: .init(latitude: 37.334_900,longitude: -122.009_020),
+final class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    @Published var region = MKCoordinateRegion(
+        center: .init(latitude: 37.334_900, longitude: -122.009_020),
         span: .init(latitudeDelta: 0.2, longitudeDelta: 0.2)
     )
+    
+    override init() {
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.setup()
+    }
+    
+    func setup() {
+        switch locationManager.authorizationStatus {
+        //If we are authorized then we request location just once, to center the map
+        case .authorizedWhenInUse:
+            locationManager.requestLocation()
+        //If we don´t, we request authorization
+        case .notDetermined:
+            locationManager.startUpdatingLocation()
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            break
+        }
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        guard .authorizedWhenInUse == manager.authorizationStatus else { return }
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Something went wrong: \(error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
+        locations.last.map {
+            region = MKCoordinateRegion(
+                center: $0.coordinate,
+                span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            )
+        }
+    }
+}
+
+struct MapView: View {
+//    @State var region = MKCoordinateRegion(
+//        center: .init(latitude: 37.334_900,longitude: -122.009_020),
+//        span: .init(latitudeDelta: 0.2, longitudeDelta: 0.2)
+//    )
+    @State var manager = LocationManager()
         
     var body: some View {
-        Map(coordinateRegion: $region)
-            .edgesIgnoringSafeArea(.all)
+//        Map(coordinateRegion: $region)
+//            .edgesIgnoringSafeArea(.all)
+        Map(coordinateRegion: $manager.region, showsUserLocation: true)
+                    .edgesIgnoringSafeArea(.all)
     }
 }
 
