ba21216ca282f0937fefb63031a6b2e1e7a82763
anthonymazzola <112509417+anthonymazzola@users.noreply.github.com>
2023-10-21 12:46:36 -0400

    Initial Commit

---
+++ MyFindMusic/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyFindMusic
+//
+//  Created by Anthony Mazzola on 10/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFindMusic/MyFindMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFindMusicApp.swift
+//  MyFindMusic
+//
+//  Created by Anthony Mazzola on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFindMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
