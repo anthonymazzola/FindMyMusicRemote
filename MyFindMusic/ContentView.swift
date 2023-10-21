21e98a17847898f27e67f07b9bbcc34de8008b4f
jamesbush <jjbush@uvm.edu>
2023-10-21 13:28:16 -0400

    Rough start framework

--- MyFindMusic/ContentView.swift
+++ MyFindMusic/ContentView.swift
@@ -1,26 +1,77 @@
 //
 //  ContentView.swift
-//  MyFindMusic
+//  test
 //
-//  Created by Anthony Mazzola on 10/21/23.
+//  Created by James  Bush on 10/19/23.
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
+                .foregroundStyle(.tint)
             Text("Hello, world!")
         }
         .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct MapView: View {
+    @State var region = MKCoordinateRegion(
+        center: .init(latitude: 37.334_900,longitude: -122.009_020),
+        span: .init(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+        
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .edgesIgnoringSafeArea(.all)
     }
 }
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+        // Implement your profile view here
+    }
+}
+
+struct SearchFriends: View {
+    var body: some View {
+        Text("Friend View")
+        // Implement your profile view here
+    }
+}
+
+struct TabViewDemo: View {
+    init(){
+        UITabBar.appearance().backgroundColor = UIColor.systemBackground
+    }
+    var body: some View {
+        TabView() {
+            MapView()
+                .tabItem() {
+                    Image(systemName: "map")
+                }
+            SearchFriends()
+                .tabItem() {
+                    Image(systemName: "figure.2.arms.open")
+                }
+            ProfileView()
+                .tabItem() {
+                    Image(systemName: "person")
+                }
+        }
+    }
+}
+
+
+#Preview {
+//    ContentView()
+    TabViewDemo()
+}
+
+
