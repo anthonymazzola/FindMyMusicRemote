ce1a80873afb153a9d423b0ad4381233143d83a3
elijahcoolidge <epcoolid@uvm.edu>
2023-11-28 11:12:59 -0500

    Fetch friend function, pulls list of friend names and displays as a button with those names

--- MyFindMusic/Core/Friends/View/FriendsView.swift
+++ MyFindMusic/Core/Friends/View/FriendsView.swift
@@ -7,35 +7,65 @@
 
 import SwiftUI
 
+
 struct FriendsView: View {
     @EnvironmentObject var viewModel: AuthViewModel
-    
+
     @State var searchText: String = ""
+    @State var friendNames: [String] = []
+
+
+
     var body: some View {
         let user = viewModel.currentUser
 
 
+
         VStack{
 
-                    Text("Friends")
-                        .font(.title)
-                        .padding()
-                    TextField("Search Here", text: self.$searchText)
-                        .padding(10)
-                        .background(Color(.systemGray5))
-                        .cornerRadius(20)
-                        .padding(.horizontal, 20)
-                    Spacer()
-                    VStack{
-                        List(user?.friends ?? ["Cant load"], id: \.self){ friend in
+            Text("Friends")
+                .font(.title)
+                .padding()
+            TextField("Search Here", text: self.$searchText)
+                .padding(10)
+                .background(Color(.systemGray5))
+                .cornerRadius(20)
+                .padding(.horizontal, 20)
+            Spacer()
+            VStack{
+                List(friendNames, id: \.self) { friendName in
                             Button(action: {
-                                                }) {
-                                                    Text(friend)
-                                                        .font(.body)
-                                                        .foregroundColor(.blue)
-                                                }
+                                // Fetch the friend's full name asynchronously
+                                print(friendName)
+                            }) {
+                                    Text(friendName)
+
+                                }
                             }
                         }
-                    }
+            }
+            .onAppear {
+                var torun = fetchFriendFullName(uid: user!.friends)
+            }
+    }
+
+    private func fetchFriendFullName(uid: [String]) -> [String] {
+        var theseFriendNames: [String] = []
+        Task {
+            for thisId in uid{
+                if let friend = await fetchFriend(uid: thisId) {
+                    print(friend.fullname)
+                    theseFriendNames.append(friend.fullname)
+                    friendNames.append(friend.fullname)
+                }
+                else {
+                    theseFriendNames.append("cant load")
+                }
+            }
+            print(theseFriendNames)
+
+        }
+        return theseFriendNames
     }
 }
+
--- MyFindMusic/Core/Friends/ViewModel/FriendsViewModel.swift
+++ MyFindMusic/Core/Friends/ViewModel/FriendsViewModel.swift
@@ -13,6 +13,28 @@ func addFriend(uid: String) async {
     
 }
 
-func fetchFriend() async {
-    guard let uid = Auth.auth().currentUser?.uid else { return }
+
+func fetchFriend(uid: String) async -> User? {
+    let usersCollection = Firestore.firestore().collection("users")
+
+    do {
+        let querySnapshot = try await Firestore.firestore().collection("users").whereField("id", isEqualTo: uid).getDocuments()
+
+        guard let userDocument = querySnapshot.documents.first else {
+            print("User document not found")
+            return nil
+        }
+
+        do {
+            let user = try userDocument.data(as: User.self)
+            return user
+        } catch {
+            print("Error decoding user document: \(error)")
+            return nil
+        }
+    } catch {
+        print("Error getting user document: \(error)")
+        return nil
+    }
 }
+
